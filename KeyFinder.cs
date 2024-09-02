7b4f93f3ab2e588c714923b8f8f301da913ed6a7
Mark Warriner <mark.warriner@madetech.com>
2024-09-02 13:12:28 +0100

    initial files

---
+++ KeyFinder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace MusicKeyFinder
+{
+	public class KeyFinder
+	{
+       string[,] keys = {
+            {"Ab", "Bbm", "Cm", "Db", "Eb", "Fm", "Gdim"},
+            {"Ab", "Bbm", "Cm", "Db", "Eb", "Fm", "Gdim"},
+            {"A", "Bm",  "C#m", "D", "E", "F#m", "G#dim"},
+            {"Bb","Cm", "Dm", "Eb","F",  "Gm",  "Adim"},
+            {"B", "C#m","D#m", "E", "F#", "G#m", "A#dim"},
+            {"C", "Dm", "Em", "F", "G", "Am", "Bdim"},
+            {"C#", "D#m", "E#m", "F#", "G#", "A#m", "B#dim"},
+            {"Db", "Ebm", "Fm", "Gb", "Ab", "Bbm", "Cdim"},
+            {"D", "Em", "F#m", "G", "A", "Bm", "C#dim"},
+            {"Eb", "Fm", "Gm", "Ab", "Bb", "Cm", "Ddim"},
+            {"E", "F#m", "G#m", "A", "B", "C#m", "D#dim"},
+            {"F", "Gm", "Am", "Bb", "C", "Dm", "Edim"},
+            {"F#", "G#m", "A#m", "B", "C#", "D#m", "E#dim"},
+            {"Gb", "Abm", "Bbm", "Cb", "Db", "Ebm", "Fdim"},
+            {"G", "Am", "Bm", "C", "D", "Em", "F#dim"},
+        };
+
+		public KeyFinder()
+		{
+
+        }
+
+        public string[] GetKeysFromChords(string[] chords)
+        {
+            var potentialkeys = new List<string>();
+            for(int i = 0; i < this.keys.GetLength(0); i++)
+            {
+                var chordsToSatisfy = new List<string>(chords);
+                for (int j = 0; j < this.keys.GetLength(1); j++)
+                {
+                    foreach(string chordToSatisfy in chords)
+                    {
+                        if (this.keys[i, j].ToLower().Equals(chordToSatisfy.ToLower()))
+                        {
+                            var indexToRemove = chordsToSatisfy.FindIndex(x => x.Equals(chordToSatisfy, StringComparison.OrdinalIgnoreCase));
+                            chordsToSatisfy.RemoveAt(indexToRemove);            
+                        }
+                    }
+                    if (chordsToSatisfy.Count == 0)
+                    {
+                        potentialkeys.Add(this.keys[i, 0]);
+                        break;
+                    }
+
+                }
+            }
+            return potentialkeys.ToArray();
+        }
+    }
+}
+
---
+++ KeyFinderTests.cs
@@ -0,0 +1,72 @@
+﻿namespace MusicKeyFinder;
+
+public class KeyFinderTests
+{
+    [Fact]
+    public void With_Chord_E_suggest_keys_E_B_A()
+    {
+        KeyFinder keyfinder = new();
+        var keys = keyfinder.GetKeysFromChords(new string[] { "E" });
+        Assert.Contains("E", keys);
+        Assert.Contains("B", keys);
+        Assert.Contains("A", keys);
+    }
+
+    [Fact]
+    public void With_Chords_In_mixed_case_suggest_correct_keys()
+    {
+        KeyFinder keyfinder = new();
+        var keys = keyfinder.GetKeysFromChords(new string[] { "c#m", "D#M", "E", "F#", "g#M", "A#DiM" });
+        Assert.Contains("B", keys);
+    }
+
+    [Fact]
+    public void With_Chord_A_Bm_suggest_keys_A_D()
+    {
+        KeyFinder keyfinder = new();
+        var keys = keyfinder.GetKeysFromChords(new string[] { "A", "Bm" });
+        Assert.True(keys.Length == 2);
+        Assert.Contains("A", keys);
+        Assert.Contains("D", keys);
+    }
+
+    [Fact]
+    public void With_Non_existant_Chord_suggests_no_keys()
+    {
+        KeyFinder keyfinder = new();
+        var keys = keyfinder.GetKeysFromChords(new string[] { "Z", "Bm" });
+        Assert.True(keys.Length == 0);
+    }
+
+    [Fact]
+    public void With_bad_Chord_combination_suggests_no_keys()
+    {
+        KeyFinder keyfinder = new();
+        var keys = keyfinder.GetKeysFromChords(new string[] { "B", "Bm" });
+        Assert.True(keys.Length == 0);
+    }
+
+    [Fact]
+    public void With_All_Chords_In_B_suggest_B()
+    { 
+        KeyFinder keyfinder = new();
+        var keys = keyfinder.GetKeysFromChords(new string[] { "C#m", "D#m", "E", "F#", "G#m", "A#dim" });
+        Assert.Contains("B", keys);
+    }
+
+    [Fact]
+    public void With_All_Chords_In_B_out_order_suggest_B()
+    {
+        KeyFinder keyfinder = new();
+        var keys = keyfinder.GetKeysFromChords(new string[] { "D#m", "E", "F#", "G#m", "A#dim", "C#m" });
+        Assert.Contains("B", keys);
+    }
+
+    [Fact]
+    public void With_All_Chords_In_G_out_of_order_suggest_G()
+    {
+        KeyFinder keyfinder = new();
+        var keys = keyfinder.GetKeysFromChords(new string[] { "G", "F#dim", "Am", "C", "D", "Bm", "Em" });
+        Assert.Contains("G", keys);
+    }
+}
